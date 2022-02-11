50af6347212a2950e8f050357bbeba2717da9552
Arthur Helfstein Fragoso <arthur@life.net.br>
2022-02-11 17:05:44 -0300

    on:error example

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+    const fallback_onerror = (e) => {
+        console.error('Some fallback')
+    }
+
+</script>
+
+<h1>Testing on:error</h1>
+
+<video autoplay muted loop>
+    <source src="some_video.webm"
+        type='video/some_unsupported_mime; codecs="some_unsupported_codec"'
+        on:error={fallback_onerror}>
+</video>
+
+
+<!-- Workaround -->
+
+<!-- <video autoplay muted loop>
+    <source src="some_video.webm"
+        type='video/some_unsupported_mime; codecs="some_unsupported_codec"'
+        onerror="console.error('Some fallback - workaround')">
+</video> -->
